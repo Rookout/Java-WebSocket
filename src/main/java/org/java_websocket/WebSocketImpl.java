25be44ae6ab4b8b7c5d16e2ff0b0bdf90a82db33
Ori Shoshan <ori.shoshan@gmail.com>
2020-03-29 16:01:45 +0300

    Capacity

--- src/main/java/org/java_websocket/WebSocketImpl.java
+++ src/main/java/org/java_websocket/WebSocketImpl.java
@@ -728,6 +728,7 @@ public class WebSocketImpl implements WebSocket {
 	private void write( ByteBuffer buf ) throws InterruptedException {
 		//log.trace( "write({}): {}", buf.remaining(), buf.remaining() > 1000 ? "too big to display" : new String( buf.array() ));
 
+		System.out.println("Remaining capacity: " + outQueue.remainingCapacity());
 		outQueue.put( buf );
 		wsl.onWriteDemand( this );
 	}
