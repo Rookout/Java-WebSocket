d9af8e5808b8cacfb92fb9ab232f98ec5af235de
benklein <ben@rookout.com>
2020-10-01 01:43:00 +0300

    relocate

---
+++ src/main/java/org/rookout/java_websocket/enums/CloseHandshakeType.java
@@ -0,0 +1,8 @@
+package org.rookout.java_websocket.enums;
+
+/**
+ * Enum which represents type of handshake is required for a close
+ */
+public enum CloseHandshakeType {
+    NONE, ONEWAY, TWOWAY
+}
